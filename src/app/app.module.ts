6643aabb66e4ab539f1812b25734125f36a0156c
Riyasheri <Riyariyaaddon2022@gmail.com>
2022-07-02 15:38:16 +0530

    addit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RemployeeComponent } from './remployee/remployee.component';
+import { SearchemployeeComponent } from './searchemployee/searchemployee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RemployeeComponent
+    RemployeeComponent,
+    SearchemployeeComponent
   ],
   imports: [
     BrowserModule,
