190332b4c4aa98515405c33be5aaf40d6ff3705f
Riyasheri <Riyariyaaddon2022@gmail.com>
2022-07-02 15:20:25 +0530

    meaasage

---
+++ src/app/remployee/remployee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-remployee',
+  templateUrl: './remployee.component.html',
+  styleUrls: ['./remployee.component.css']
+})
+export class RemployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
